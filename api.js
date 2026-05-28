a40fc9511e582c20747d9f4e47636941442e612a
shady2409 <sriramshady24@gmail.com>
2026-05-28 10:53:32 +0530

    Updated API URL

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://findit-backend-qh3s.onrender.com/api';
 
 // ---------- AUTH ----------
 async function apiRegister(name, email, password, collegeId, phone) {
